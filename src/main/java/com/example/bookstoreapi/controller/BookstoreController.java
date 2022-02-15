f49f52ee3c491b500016928d43662316c8389bcb
Huy Nguyen Xuan <dxhuy26@gmail.com>
2022-02-16 04:56:58 +0700

    up

--- src/main/java/com/example/bookstoreapi/controller/BookstoreController.java
+++ src/main/java/com/example/bookstoreapi/controller/BookstoreController.java
@@ -4,6 +4,7 @@ import com.example.bookstoreapi.exception.ResourceNotFoundException;
 import com.example.bookstoreapi.model.BookStore;
 import com.example.bookstoreapi.repository.BookstoreRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -27,5 +28,26 @@ public class BookstoreController {
         BookStore bookStore =bookstoreRepository.findById(id)
                 .orElseThrow(()-> new ResourceNotFoundException("Book not found: " + id));
         return ResponseEntity.ok(bookStore);
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<BookStore> updateBook(@PathVariable Long id, @RequestBody BookStore bookdetails){
+        BookStore updateBook = bookstoreRepository.findById(id)
+                .orElseThrow(()-> new ResourceNotFoundException("Book not found: " + id));
+        updateBook.setName(bookdetails.getName());
+        updateBook.setAuthor(bookdetails.getAuthor());
+        updateBook.setPublication(bookdetails.getPublication());
+        updateBook.setPrice(bookdetails.getPrice());
+
+        bookstoreRepository.save(updateBook);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<HttpStatus> deleteBook(@PathVariable Long id){
+        BookStore bookStore =bookstoreRepository.findById(id)
+                .orElseThrow(()-> new ResourceNotFoundException("Book not found: " + id));
+        bookstoreRepository.delete(bookStore);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
